e56d7fcd7cfa72223da52589e0c0d6f1af71b6cf
legoraft <50689727+legoraft@users.noreply.github.com>
2023-10-30 21:37:12 +0100

    Well, I have a somewhat functional gui

--- src/client/java/com/sophswrath/SophsWrathClient.java
+++ src/client/java/com/sophswrath/SophsWrathClient.java
@@ -1,10 +1,32 @@
 package com.sophswrath;
 
+import com.sophswrath.gui.QuestLineGui;
+import io.github.cottonmc.cotton.gui.client.CottonClientScreen;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.client.util.math.MatrixStack;
+import org.lwjgl.glfw.GLFW;
 
 public class SophsWrathClient implements ClientModInitializer {
+
+	private static KeyBinding toggleQuests;
+
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		toggleQuests = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"key.sophswrath.togglequestline",
+				InputUtil.Type.KEYSYM,
+				GLFW.GLFW_KEY_K,
+				"category.sophswrath"
+		));
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			if (toggleQuests.wasPressed()) {
+				client.setScreen(new CottonClientScreen(new QuestLineGui()));
+			}
+		});
 	}
 }
---
+++ src/client/java/com/sophswrath/gui/QuestLineGui.java
@@ -0,0 +1,24 @@
+package com.sophswrath.gui;
+
+import io.github.cottonmc.cotton.gui.client.BackgroundPainter;
+import io.github.cottonmc.cotton.gui.client.LightweightGuiDescription;
+import io.github.cottonmc.cotton.gui.widget.*;
+import io.github.cottonmc.cotton.gui.widget.data.Insets;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Identifier;
+
+public class QuestLineGui extends LightweightGuiDescription {
+
+    public QuestLineGui() {
+        WGridPanel root = new WGridPanel();
+        setRootPanel(root);
+        root.setSize(252, 140);
+        root.setInsets(Insets.ROOT_PANEL);
+
+        root.setBackgroundPainter(BackgroundPainter.createNinePatch(new Identifier("minecraft:textures/block/dirt.png")));
+        root.getBackgroundPainter().paintBackground(new MatrixStack(), 0, 0, root);
+
+        root.validate(this);
+    }
+
+}
