7d520bc8645cf3d9c019e1403e89b28f855e863d
legoraft <50689727+legoraft@users.noreply.github.com>
2023-10-31 16:12:23 +0100

    Reset the panel to the default

--- src/client/java/com/sophswrath/gui/QuestLineGui.java
+++ src/client/java/com/sophswrath/gui/QuestLineGui.java
@@ -4,8 +4,6 @@ import io.github.cottonmc.cotton.gui.client.BackgroundPainter;
 import io.github.cottonmc.cotton.gui.client.LightweightGuiDescription;
 import io.github.cottonmc.cotton.gui.widget.*;
 import io.github.cottonmc.cotton.gui.widget.data.Insets;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.util.Identifier;
 
 public class QuestLineGui extends LightweightGuiDescription {
 
@@ -20,7 +18,7 @@ public class QuestLineGui extends LightweightGuiDescription {
 
     @Override
     public void addPainters() {
-        getRootPanel().setBackgroundPainter(BackgroundPainter.createNinePatch(new Identifier("minecraft:textures/block/dirt.png")));
+        getRootPanel().setBackgroundPainter(BackgroundPainter.VANILLA);
     }
 
 }
