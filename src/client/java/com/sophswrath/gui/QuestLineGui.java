2707aac66ae42401a6e735646df0b8462ce6c468
legoraft <50689727+legoraft@users.noreply.github.com>
2023-11-05 18:51:02 +0100

    Well, we finally have some multi-screen action

--- src/client/java/com/sophswrath/gui/QuestLineGui.java
+++ src/client/java/com/sophswrath/gui/QuestLineGui.java
@@ -2,8 +2,10 @@ package com.sophswrath.gui;
 
 import io.github.cottonmc.cotton.gui.client.BackgroundPainter;
 import io.github.cottonmc.cotton.gui.client.LightweightGuiDescription;
+import io.github.cottonmc.cotton.gui.client.ScreenDrawing;
 import io.github.cottonmc.cotton.gui.widget.*;
 import io.github.cottonmc.cotton.gui.widget.data.Insets;
+import net.minecraft.util.Identifier;
 
 public class QuestLineGui extends LightweightGuiDescription {
 
@@ -13,12 +15,17 @@ public class QuestLineGui extends LightweightGuiDescription {
         root.setSize(252, 140);
         root.setInsets(Insets.ROOT_PANEL);
 
+
         root.validate(this);
     }
 
     @Override
     public void addPainters() {
-        getRootPanel().setBackgroundPainter(BackgroundPainter.VANILLA);
+        BackgroundPainter backgroundPainter = ((context, left, top, root) -> {
+            ScreenDrawing.drawGuiPanel(context, left, top, rootPanel.getWidth(), rootPanel.getHeight());
+            ScreenDrawing.texturedGuiRect(context, 1, 1, rootPanel.getWidth() - rootPanel.getWidth() / 8, rootPanel.getHeight() - rootPanel.getHeight() / 8, new Identifier("minecraft:textures/block/dirt.png"), 0xFFFFFFFF);
+        });
+        rootPanel.setBackgroundPainter(backgroundPainter);
     }
 
 }
