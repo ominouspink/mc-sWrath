bb990cabf40b28e8e57e1d124d24b7c557afcf61
legoraft <50689727+legoraft@users.noreply.github.com>
2023-11-06 22:18:54 +0100

    FINALLY

--- src/client/java/com/sophswrath/gui/questline/QuestScreen.java
+++ src/client/java/com/sophswrath/gui/questline/QuestScreen.java
@@ -5,11 +5,20 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.advancement.AdvancementTab;
+import net.minecraft.client.texture.TextureManager;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.MathHelper;
+
+import java.util.Objects;
 
 @Environment(EnvType.CLIENT)
 public class QuestScreen extends Screen {
+
+    private double originX;
+    private double originY;
+    private boolean initialized;
 
     public QuestScreen() {
         super(Text.translatable("quests.title"));
@@ -17,7 +26,7 @@ public class QuestScreen extends Screen {
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-        renderBackground(context, this.width / 2 - 124, this.height / 2 - 68);
+        this.drawBackground(context, this.width / 2 - 117, this.height / 2 - 52);
         drawQuestWindow(context, this.width / 2 - 126, this.height / 2 - 70);
     }
 
@@ -27,8 +36,34 @@ public class QuestScreen extends Screen {
         context.drawText(this.textRenderer, super.title, x + 8, y + 6, 4210752, false);
     }
 
-    public void renderBackground(DrawContext context, int x, int y) {
-        Identifier backgroundTexture = new Identifier("textures/block/dirt.png");
-        context.drawGuiTexture(backgroundTexture, x,  y, 248, 136);
+    public void drawBackground(DrawContext context, int x, int y) {
+        if (!this.initialized) {
+            this.originX = (double)(117 - 34 / 2);
+            this.originY = (double)(56 - 12 / 2);
+            this.initialized = true;
+        }
+
+        context.enableScissor(x, y, x + 234, y + 113);
+        context.getMatrices().push();
+        context.getMatrices().translate((float)x, (float)y, 0.0F);
+        Identifier identifier = new Identifier("minecraft:textures/block/dirt.png");
+        int i = MathHelper.floor(this.originX);
+        int j = MathHelper.floor(this.originY);
+        int k = i % 16;
+        int l = j % 16;
+
+        for(int m = -1; m <= 15; ++m) {
+            for(int n = -1; n <= 8; ++n) {
+                context.drawTexture(identifier, k + 16 * m, l + 16 * n, 0.0F, 0.0F, 16, 16, 16, 16);
+            }
+        }
+
+        context.getMatrices().pop();
+        context.disableScissor();
     }
+
+    private void move() {
+
+    }
+
 }
