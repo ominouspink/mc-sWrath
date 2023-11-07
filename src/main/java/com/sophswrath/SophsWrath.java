dbe75583a03398c23f7d1c6a11200cd87e319623
legoraft <50689727+legoraft@users.noreply.github.com>
2023-11-07 11:14:03 +0100

    Just use the integrated api...

--- src/main/java/com/sophswrath/SophsWrath.java
+++ src/main/java/com/sophswrath/SophsWrath.java
@@ -9,7 +9,7 @@ public class SophsWrath implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("sophs-wrath");
+    public static final Logger LOGGER = LoggerFactory.getLogger("data/sophs-wrath");
 
 	@Override
 	public void onInitialize() {
